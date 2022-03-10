bf1faf0eb8f938b45b7e1d3c95b98549b6c6cf30
Higher Light <30793074+FreeLight21@users.noreply.github.com>
2022-03-10 21:09:10 +0800

    Update SplitArray.java

--- src/SplitArray.java
+++ src/SplitArray.java
@@ -1,6 +1,7 @@
 public class SplitArray {
     public int splitArray(int[] nums, int m) {
          System.out.println("测试一些远程pull");
+         Sytem.out.println("远程库codehub,main分支，pull");
          return 0;
     }
 
