6b1f53731e09117cc6d0d06b42f864cf8d65f6cb
LightHigher <1651555193@qq.com>
2022-03-10 19:42:27 +0800

    "提交leetcode代码,fourth-commit"

--- src/NextGreatElement.java
+++ src/NextGreatElement.java
@@ -21,6 +21,7 @@ public class NextGreatElement {
             String a = "abc";
             System.out.println("hello,change");
             System.out.println("hot-fix remove");
+            System.out.println("xiugai ");
         }
         return res;
     }
